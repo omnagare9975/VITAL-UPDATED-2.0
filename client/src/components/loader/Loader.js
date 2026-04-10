d12b492f2b599ca940aeb6af46036abdbfbc35fe
omnagare <omnagare07@gmail.com>
2026-04-10 14:32:35 +0530

    chagnes added in vital

--- client/src/components/loader/Loader.js
+++ client/src/components/loader/Loader.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './loader.css';
 
-const Loader = () => {
-    return (
-        <>
-            <div class="overlay" id="overlay"></div>
-            <div class="loader">
-                <div class="loader-cube">
-                    <div class="face"></div>
-                    <div class="face"></div>
-                    <div class="face"></div>
-                    <div class="face"></div>
-                    <div class="face"></div>
-                    <div class="face"></div>
-                </div>
-            </div>
-        </>
-    )
-}
+const Loader = ({ loading = true, text = 'Loading...' }) => {
+  if (!loading) return null;
+
+  return (
+    <div className="loader-overlay" role="status" aria-live="polite" aria-busy="true">
+      <div className="loader-container">
+        <div className="loader-cube">
+          <span className="face face-1"></span>
+          <span className="face face-2"></span>
+          <span className="face face-3"></span>
+          <span className="face face-4"></span>
+          <span className="face face-5"></span>
+          <span className="face face-6"></span>
+        </div>
+        <p className="loader-text">{text}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Loader;
